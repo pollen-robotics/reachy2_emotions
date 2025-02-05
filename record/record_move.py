34916f882cc871ed1dc99a23152dedab7e6fb07a
RemiFabre <remifabre1800@gmail.com>
2025-02-05 13:33:41 +0100

    addind default version of scripts

---
+++ record/record_move.py
@@ -0,0 +1,84 @@
+import argparse
+import datetime
+import json
+import os
+import time
+
+from reachy2_sdk import ReachySDK  # type: ignore
+
+
+def main(ip: str, filename: str, freq: int):
+    reachy = ReachySDK(host=ip)
+
+    data: dict = {
+        "time": [],
+        "l_arm": [],
+        "l_hand": [],
+        "r_arm": [],
+        "r_hand": [],
+        "head": [],
+    }
+
+    input("Press Enter to start the recording.")
+
+    try:
+        t0 = time.time()
+        print("Recording in progress, press Ctrl+C to stop")
+
+        while True:
+            l_arm = reachy.l_arm.get_current_positions()
+            r_arm = reachy.r_arm.get_current_positions()
+            head = reachy.head.get_current_positions()
+            l_hand = reachy.l_arm.gripper.get_current_opening()
+            r_hand = reachy.r_arm.gripper.get_current_opening()
+
+            data["time"].append(time.time() - t0)
+            data["l_arm"].append(l_arm)
+            data["l_hand"].append(l_hand)
+            data["r_arm"].append(r_arm)
+            data["r_hand"].append(r_hand)
+            data["head"].append(head)
+
+            time.sleep(1 / freq)
+
+    except KeyboardInterrupt:
+        file_path = os.path.join("recordings", filename)
+        with open(file_path, "w") as f:
+            json.dump(data, f, indent=4)
+        print(
+            f"Data saved to {filename}, time of recording :",
+            time.time() - t0
+        )
+
+
+if __name__ == "__main__":
+    d = datetime.datetime.now()
+    default_filename = f'recording_{d.strftime("%m%d_%H%M")}.json'
+    parser = argparse.ArgumentParser(
+        description="Record the movements of Reachy."
+    )
+
+    parser.add_argument(
+        "--ip",
+        type=str,
+        default="localhost",
+        help="IP address of the robot",
+    )
+
+    parser.add_argument(
+        "--filename",
+        type=str,
+        default=default_filename,
+        help="Name of the file to save the data",
+    )
+
+    parser.add_argument(
+        "--freq",
+        type=int,
+        default=100,
+        help="Frequency of the recording",
+    )
+
+    args = parser.parse_args()
+
+    main(args.ip, args.filename, args.freq)
